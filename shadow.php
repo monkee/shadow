852eaddf5b6543b65b85e6902012b86840a1a4f8
monkee <zomboo1@126.com>
2013-03-17 21:45:44 +0800

    add shadow.php

---
+++ shadow.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 基本的shadow载入文件
+ * 
+ * 包含：
+ * 1. class autoload规则
+ * 2. 定义基本环境常量
+ * 3. 定义基本类和基本异常处理
+ * 
+ * @author monkee<zomboo1@126.com>
+ * @copyright 2013-2014
+ * @package shadow
+ * @version 0.1.0
+ */
